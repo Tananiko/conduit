f3ee5e5bbd5639c798122e412a2cc40684518e71
Tananiko <tanczosaniko@yahoo.com>
2021-08-10 22:48:36 +0200

    Conduitremek commit - TC00-TC01-TC02-TC03-TC04-TC05-TC11

--- test/test_TC05_SajatProfilSzerk.py
+++ test/test_TC05_SajatProfilSzerk.py
@@ -27,7 +27,7 @@ class TestEditProfile(object):
         conduit_login(self.driver)
         WebDriverWait(
             self.driver, 25)
-        settings = self.driver.find_element_by_xpath("//a[@class='nav-link router-link-exact-active active']")
+        settings = self.driver.find_element_by_xpath('//a[@href="#/settings"]')
         settings.click()
         profile_picture = self.driver.find_element_by_xpath('//input[@placeholder="URL of profile picture"]')
         assert profile_picture == 'https://static.productionready.io/images/smiley-cyrus.jpg'
