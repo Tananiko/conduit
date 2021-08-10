6394148fa9706d80b773739152d29a041f32d9eb
Tananiko <tanczosaniko@yahoo.com>
2021-08-10 23:00:22 +0200

    Conduitremek commit - TC00-TC01-TC02-TC03-TC04-TC05-TC06-TC11

--- test/test_TC06_AdatsorozatFile.py
+++ test/test_TC06_AdatsorozatFile.py
@@ -4,12 +4,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import datetime
-import time
 import random
 import string
 import csv
 from selenium.webdriver.common.keys import Keys
+from conduit_data import conduit_login
 
 class TestFileUpload(object):
 
@@ -22,33 +21,21 @@ class TestFileUpload(object):
     def teardown(self):
         self.driver.quit()
 
-    def test_website(self):
-        self.driver.maximize_window()
-        time.sleep(2)
-        assert self.driver.find_element_by_xpath('//a[@class="navbar-brand router-link-exact-active router-link-active"]'
-                                                 ).text == "conduit"
 
-    def test_navigate_to_login(self):
-        self.driver.find_element_by_xpath('//a[contains(text(),"Sign in")]').click()
-        self.driver.find_element_by_xpath('//input[@placeholder="Email"]').send_keys("Aniko1@gmail.com")
-        self.driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys("Tananiko-1")
-        self.driver.find_element_by_xpath("//button[normalize-space()='Sign in']").click()
-        self.driver.find_elements_by_css_selector('li.nav-item')
-
-        WebDriverWait(
-            self.driver, 50).until(
-            EC.visibility_of_element_located((By.LINK_TEXT, "A1"))
-        )
     def test_create_new_article(self):
+        conduit_login(self.driver)
+        WebDriverWait(
+            self.driver, 25)
+
         self.rand_string = 'Apple'.join(random.choices(string.ascii_uppercase + string.digits, k=8))
         self.driver.find_element_by_xpath('//a[@href="#/editor"]')
         WebDriverWait(
-            self.driver, 50).until(
+            self.driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, '//a[@href="#/editor"]'))
         ).click()
 
         WebDriverWait(
-            self.driver, 50).until(
+            self.driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, '//input[@placeholder="Article Title"]'))
         )
         self.driver.find_element_by_xpath('//input[@placeholder="Article Title"]').send_keys("Apple")
@@ -59,34 +46,59 @@ class TestFileUpload(object):
         self.driver.find_element_by_xpath('//form/button').click()
 
         WebDriverWait(
-            self.driver, 50).until(
+            self.driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div[1]/div/h1'))
         )
         article_appearance = self.driver.find_element_by_xpath('//*[@id="app"]/div/div[1]/div/h1')
         assert article_appearance == "Apple"
 
         WebDriverWait(
-            self.driver, 50)
+            self.driver, 20)
 
     def test_create_comment_upload_from_file(self):
 
         self.driver.find_element_by_xpath("//textarea[@placeholder='Write a comment...']")
         WebDriverWait(
-            self.driver, 50).until(
+            self.driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, "//textarea[@placeholder='Write a comment...']"))
         )
-        with open('../data.csv', 'r', encoding="utf-8") as csvfile:
+        with open('data.csv', 'r', encoding="utf-8") as csvfile:
             csv_reader = (csvfile)
             next(csv_reader)
             for row in csv_reader:
-                self.driver.find_element_by_xpath('//textarea[@placeholder="Write a comment..."]').send_keys(row[0])
-                self.driver.find_element_by_xpath('//form/button').click()
+                element.send_keys(row)
+                self.driver.find_element_by_xpath('//button[text()="Post Comment"]').click()
 
         WebDriverWait(
-            self.driver, 50).until(
+            self.driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, '//p[normalize-space()="Tasty"]'))
 
         )
         comment = self.driver.find_element_by_class_name("card-text")
         last_comment = comment[0].text
+        WebDriverWait(
+            self.driver, 10
+        )
         assert last_comment == "Tasty"
+
+    # def test_create_comment_upload_from_file(self):
+    #     con_login(self.driver)
+    #     time.sleep(2)
+    #     self.driver.find_element_by_xpath('//h1[text()="Lorem ipsum dolor sit amet"]').click()
+    #     element = WebDriverWait(
+    #         self.driver, 10).until(
+    #         EC.visibility_of_element_located((By.XPATH, "//textarea[@placeholder='Write a comment...']"))
+    #     )
+    #     with open('data.csv', 'r', encoding="utf-8") as csvfile:
+    #         csv_reader = (csvfile)
+    #         for row in csv_reader:
+    #             element.send_keys(row)
+    #             self.driver.find_element_by_xpath('//button[text()="Post Comment"]').click()
+    #     WebDriverWait(
+    #         self.driver, 10).until(
+    #         EC.visibility_of_element_located((By.XPATH, '//p[normalize-space()="Tasty,"]'))
+    #     )
+    #     comment = self.driver.find_elements_by_class_name("card-text")
+    #     last_comment = comment[0].text
+    #     time.sleep(1)
+    #     assert last_comment == "Tasty,"
