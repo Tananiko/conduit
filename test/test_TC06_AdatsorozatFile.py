ddac9dd9e47920f811e7fecc694a67eae5b1475d
Tananiko <tanczosaniko@yahoo.com>
2021-08-10 23:43:46 +0200

    Conduitremek commit - TC00-TC01-TC02-TC03-TC04-TC05-TC06-TC11

--- test/test_TC06_AdatsorozatFile.py
+++ test/test_TC06_AdatsorozatFile.py
@@ -22,63 +22,28 @@ class TestFileUpload(object):
         self.driver.quit()
 
 
-    def test_create_new_article(self):
+    def test_upload_new_article_from_file(self):
         conduit_login(self.driver)
         WebDriverWait(
-            self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, "//a[@href='#/editor']"))
-        )
-        self.driver.find_element_by_xpath("//a[@href='#/editor']").click()
+            self.driver, 25)
 
-        WebDriverWait(
-            self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, '//input[@placeholder="Article Title"]'))
-        )
-        title = self.driver.find_element_by_xpath('//input[@placeholder="Article Title"]')
-        title.send_keys("Apple has two benefits")
-        about = self.driver.find_element_by_xpath('//input[contains(@placeholder,"What")]')
-        about.send_keys("Apple")
-        article = self.driver.find_element_by_xpath('//textarea[contains(@placeholder,"Write your")]')
-        article.send_keys("Apple has two benefits and an apple a day keeps the doctor away")
-        tag = self.driver.find_element_by_xpath('//input[contains(@placeholder,"tags")]')
-        tag.send_keys('Apple' + Keys.TAB)
-        self.driver.find_element_by_xpath('//button[contains(text(),"Publish")]').click()
-
-        WebDriverWait(
-            self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, "//h1[normalize-space()='Apple']"))
-        )
-
-        article_appearance = self.driver.find_element_by_xpath('//h1')
-        assert article_appearance.text == "Apple"
-
-        WebDriverWait(
-            self.driver, 30)
-
-    def test_create_comment_upload_from_file(self):
-        self.driver.find_element_by_xpath('//h1[text()="Apple has two benefits"]').click()
-        element = WebDriverWait(
-            self.driver, 20).until(
-            EC.visibility_of_element_located((By.XPATH, "//textarea[@placeholder='Write a comment...']"))
-        )
         with open('data.csv', 'r', encoding="utf-8") as csvfile:
             csv_reader = (csvfile)
             next(csv_reader)
             for row in csv_reader:
-                element.send_keys(row)
-                self.driver.find_element_by_xpath('//button[text()="Post Comment"]').click()
+                upload_new_article(self.driver, row[0], row[1], row[2], row[3], row[4])
 
+        WebDriverWait(
+            self.driver, 10
+        )
+        title_new_article = self.driver.find_element_by_xpath('//h1')
         WebDriverWait(
             self.driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, '//p[normalize-space()="Tasty"]'))
 
         )
-        comment = self.driver.find_element_by_class_name("card-text")
-        last_comment = comment[0].text
-        WebDriverWait(
-            self.driver, 10
-        )
-        assert last_comment == "Tasty"
+        assert title_new_article.text == row[0]
+
 
     # def test_create_comment_upload_from_file(self):
     #     con_login(self.driver)
