93ba375c8d69230072b9f05acd086bc8d04589ec
Tananiko <tanczosaniko@yahoo.com>
2021-08-11 23:25:44 +0200

    Conduitremek commit TC10

--- test/test_TC10_AdatlementesFeluletrol.py
+++ test/test_TC10_AdatlementesFeluletrol.py
@@ -1,7 +1,5 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
