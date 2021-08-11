b88e20b6a91f7618764a209a85d0a26c05607265
Tananiko <tanczosaniko@yahoo.com>
2021-08-11 12:52:44 +0200

    Conduitremek commit - TC03- Login

--- test/test_TC03_Login.py
+++ test/test_TC03_Login.py
@@ -20,29 +20,6 @@ class TestLoginConduit(object):
 
     def teardown(self):
         self.driver.quit()
-    #
-    # def test_website(self):
-    #     self.driver.maximize_window()
-    #     time.sleep(2)
-    #     assert self.driver.find_element_by_xpath('//a[@class="navbar-brand router-link-exact-active router-link-active"]'
-    #                                              ).text == "conduit"
-    #
-    # def test_registration(self):
-    #
-    #     self.driver.find_element_by_xpath('/html/body//a[contains(@href,"register")]').click()
-    #     self.driver.find_element_by_xpath('//input[@placeholder="Username"]').send_keys("A1")
-    #     self.driver.find_element_by_xpath('//input[@placeholder="Email"]').send_keys("Aniko1@gmail.com")
-    #     self.driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys("Tananiko-1")
-    #     self.driver.find_element_by_xpath('//button[normalize-space()="Sign up"]').click()
-    #
-    #     element = WebDriverWait(
-    #         self.driver, 50).until(
-    #         EC.visibility_of_element_located((By.XPATH, "//button[normalize-space()='OK']"))
-    #     )
-    #
-    #     self.driver.find_element_by_css_selector('.swal-title').text == "Welcome!"
-    #     self.driver.find_element_by_css_selector('.swal-button.swal-button--confirm').click()
-    #     self.driver.find_elements_by_css_selector('li.nav-item')
 
     def test_navigate_to_login(self):
         self.driver.find_element_by_xpath('//a[contains(text(),"Sign in")]').click()
