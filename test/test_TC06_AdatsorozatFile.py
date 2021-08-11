78d9cbedfd8488a6a8072105983e8165dd18803c
Tananiko <tanczosaniko@yahoo.com>
2021-08-11 06:54:39 +0200

    Conduitremek commit - TC00-TC01-TC02-TC03-TC04-TC05-TC06-TC11

--- test/test_TC06_AdatsorozatFile.py
+++ test/test_TC06_AdatsorozatFile.py
@@ -24,26 +24,24 @@ class TestFileUpload(object):
 
     def test_upload_new_article_from_file(self):
         conduit_login(self.driver)
-        WebDriverWait(
-            self.driver, 25)
-
-        with open('data.csv', 'r', encoding="utf-8") as csvfile:
-            csv_reader = (csvfile)
-            next(csv_reader)
-            for row in csv_reader:
-                upload_new_article(self.driver, row[0], row[1], row[2], row[3], row[4])
-
-        WebDriverWait(
-            self.driver, 10
-        )
-        title_new_article = self.driver.find_element_by_xpath('//h1')
-        WebDriverWait(
-            self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, '//p[normalize-space()="Tasty"]'))
-
-        )
-        assert title_new_article.text == row[0]
-
+        self.driver.find_element_by_xpath('//h1[text()="Lorem ipsum dolor sit amet"]').click()
+        #     element = WebDriverWait(
+        #         self.driver, 10).until(
+        #         EC.visibility_of_element_located((By.XPATH, "//textarea[@placeholder='Write a comment...']"))
+        #     )
+        #     with open('data.csv', 'r', encoding="utf-8") as csvfile:
+        #         csv_reader = (csvfile)
+        #         for row in csv_reader:
+        #             element.send_keys(row)
+        #             self.driver.find_element_by_xpath('//button[text()="Post Comment"]').click()
+        #     WebDriverWait(
+        #         self.driver, 10).until(
+        #         EC.visibility_of_element_located((By.XPATH, '//p[normalize-space()="Tasty,"]'))
+        #     )
+        #     comment = self.driver.find_elements_by_class_name("card-text")
+        #     last_comment = comment[0].text
+        #     time.sleep(1)
+        #     assert last_comment == "Tasty,"
 
     # def test_create_comment_upload_from_file(self):
     #     con_login(self.driver)
