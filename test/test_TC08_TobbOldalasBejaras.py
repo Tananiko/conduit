09f2136d61cfc7d40722835ea8b3818b5eccfb80
Tananiko <tanczosaniko@yahoo.com>
2021-08-12 22:40:24 +0200

    Conduitremek commit TC08

--- test/test_TC08_TobbOldalasBejaras.py
+++ test/test_TC08_TobbOldalasBejaras.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
@@ -30,4 +27,3 @@ class TestPaginationConduit(object):
                 page.click()
             else:
                 assert page.text == length
-
